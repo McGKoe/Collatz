14e044789c3b9f5fd997416bf0e88c315c143720
Your Name <koenig_mm@cobra.cs.mercer.edu>
2022-11-26 11:45:05 -0500

    Lisp With Recursion

---
+++ Lisp/recursed/collatz.lisp
@@ -0,0 +1,125 @@
+#!/usr/bin/sbcl --script
+
+
+(defstruct cell ;structure holds the happy number and its norm
+        num
+        seqlength
+)
+
+(defun makeCell (n seq) ; creates and returns a new cell
+	(let ((temp (make-cell)))
+		(setf (cell-num temp) n)
+		(setf (cell-seqlength temp) seq)
+
+		(return-from makeCell temp)
+	)
+)
+
+; determines length of collatz sequence recursively
+(defun collatz (n)
+	(progn
+	  (cond
+	    ((= n 1) (return-from collatz 0))
+	    ((= (mod n 2) 0) (return-from collatz (+ 1 (collatz (/ n 2)))))
+	    ((= (mod n 2) 1) (return-from collatz (+ 1 (collatz (+ 1 (* n 3))))))))
+
+)
+
+
+
+
+(progn
+(defparameter *setofcells* ()) ;; declares an empty set
+
+(defvar num1)
+(defvar num2)
+
+
+;;take command line input
+(setf num1 (parse-integer (nth 1 sb-ext:*posix-argv*)) )
+
+;take second command line input
+(setf num2 (parse-integer (nth 2 sb-ext:*posix-argv*)) )
+
+
+
+;;declare and set first number to find collatz
+(defvar begin)
+
+(setf begin num1)
+
+(defvar seq)
+(setf seq 0)
+
+;; find scollayz sequence for each number between num1 and num2
+(loop for i from num1 below num2 do
+
+	(setf seq 0)
+
+	(setf begin i)
+
+
+	;;collatz sequence length of begin
+	(setf seq (collatz begin))
+
+
+
+	;;adds next cell to array
+	(setf *setofcells* (adjoin (makeCell i seq) *setofcells*))
+
+)
+
+(terpri)
+
+(defparameter *final* ())
+
+;sort array by collatz sequence length
+(setf *final* (sort  *setofcells* #'> :key #'cell-seqlength))
+
+;remove duplicates
+
+(delete-duplicates *final* :key #'cell-seqlength)
+
+
+
+
+(defparameter *final2* ())
+
+(princ "Sorted based on sequencce length: ")
+(terpri)
+
+
+(loop for item in *final*
+	for i from 1 to 10
+	do (format t "~d 		~d~%" (cell-num item) (cell-seqlength item))
+
+	;;adds first ten of final to final2
+	(setf *final2* (adjoin item *final2*))	
+)
+
+;sort final2 array by size of number
+(setf *final2* (sort  *final2* #'> :key #'cell-num))
+
+
+(princ "Sorted based on integer size: ")
+(terpri)
+
+(loop for item in *final2*
+	for i from 1 to 10
+	do (format t "~d 		~d~%" (cell-num item) (cell-seqlength item))
+)
+
+
+
+
+
+
+
+
+
+
+
+
+)
+
+
